7fcde485a5abec346703ad0aff99c94e5f1a82db
Bluscream <bluscream@users.noreply.github.com>
2026-06-14 19:48:38 +0200

    Add None preset and decompiled preset modes 1-9 to lovespouse_muse_fan

--- components/lovespouse_muse_ble/fan/lovespouse_muse_fan.cpp
+++ components/lovespouse_muse_ble/fan/lovespouse_muse_fan.cpp
@@ -7,7 +7,7 @@ namespace lovespouse_muse_ble {
 static const char *TAG = "lovespouse_muse_ble.fan";
 
 void LovespouseMuseBleFan::setup() {
-  this->set_supported_preset_modes({"Fast Pulse", "Short Medium", "Ramp Up", "Alternating"});
+  this->set_supported_preset_modes({"None", "Preset 1", "Preset 2", "Preset 3", "Short Medium", "Fast Pulse", "Preset 6", "Preset 7", "Ramp Up", "Alternating"});
 }
 
 void LovespouseMuseBleFan::dump_config() {
@@ -36,12 +36,22 @@ void LovespouseMuseBleFan::control(const fan::FanCall &call) {
 void LovespouseMuseBleFan::write_state_() {
   uint8_t raw_cmd = 0x00;
   if (this->state) {
-    if (this->has_preset_mode()) {
+    if (this->has_preset_mode() && this->get_preset_mode() != "None") {
       auto mode = this->get_preset_mode();
-      if (mode == "Fast Pulse") {
-        raw_cmd = 0x05;
+      if (mode == "Preset 1") {
+        raw_cmd = 0x01;
+      } else if (mode == "Preset 2") {
+        raw_cmd = 0x02;
+      } else if (mode == "Preset 3") {
+        raw_cmd = 0x03;
       } else if (mode == "Short Medium") {
         raw_cmd = 0x04;
+      } else if (mode == "Fast Pulse") {
+        raw_cmd = 0x05;
+      } else if (mode == "Preset 6") {
+        raw_cmd = 0x06;
+      } else if (mode == "Preset 7") {
+        raw_cmd = 0x07;
       } else if (mode == "Ramp Up") {
         raw_cmd = 0x08;
       } else if (mode == "Alternating") {
